cef510e8ce929b8fee9731f78ceb0be44cc7df05
jeromeharrison <jeromeharrison06@gmail.com>
2016-05-31 15:08:32 -0400

    Bdd_sync
    rien à dire

--- Base de données/Classes_v2 (Manager)/EquipementManager.py
+++ Base de données/Classes_v2 (Manager)/EquipementManager.py
@@ -1,18 +1,18 @@
 # coding=utf-8
 from tinydb import *
-from bbd_equipement import yamlStorage
+from yamlStorage import YAMLStorage
 import re
 
 
 class EquipementManager:
-    def __init__(self, pathname, archive_id_eq_pathname):
+    def __init__(self, pathname):
         self._pathname = pathname                               # pathname de la base de données des équipements
-        self._nextID = 0   # pathname de la base de données de l'archive des équip.
+        #self._nextID = 1   # pathname de la base de données de l'archive des équip.
         # nextID à mettre dans le fichier de configuration
 
     def AjouterEquipement(self, dictio):
         #print('ouverture de la bdd')
-        db1 = TinyDB(self._pathname, storage=yamlStorage.YAMLStorage)        # data base des équipements
+        db1 = TinyDB(self._pathname, storage=YAMLStorage)        # data base des équipements
         #print('Attribution du ID')
         id_eq = self._ObtenirProchainID()                                   # id du nouvel équipement
         #print(str(id_eq))
@@ -23,37 +23,48 @@ class EquipementManager:
 
     def SupprimerEquipement(self, id_supp):
         Equipement = Query()
-        db = TinyDB(self._pathname, storage=yamlStorage.YAMLStorage)        # data base des équipements
+        db = TinyDB(self._pathname, storage=YAMLStorage)        # data base des équipements
         result = db.remove(Equipement['ID'] == id_supp)                              # suppression de l'équipement (à voir...)
         return result
 
     def RechercherEquipement(self, regex_dict):
-        db = TinyDB(self._pathname, storage=yamlStorage.YAMLStorage)
-
+        db = TinyDB(self._pathname, storage=YAMLStorage)
         recherche = Query()
-        
         firstEntry = True
-        for key,value in regex_dict:
+        for key, value in regex_dict.items():
             if firstEntry:
-                queryUser = recherche[key].matches(value)
+                queryUser = (recherche[key].matches(value))
                 firstEntry = False
             else:
-                queryUser = & recherche[key].matches(value)
+                queryUser = (queryUser) & (recherche[key].matches(value))
         result = db.search(queryUser)
         return result
 
+
     def ModifierEquipement(self, id_modif, dict_modif):
         Equipement = Query()
-        db = TinyDB(self._pathname, storage=yamlStorage.YAMLStorage)        # data base des équipements
+        db = TinyDB(self._pathname, storage=YAMLStorage)        # data base des équipements
         # Verifier que tout ce qui est dans dict_modif est conforme à la forme d'un équipement et COMPLET
         db.update(dict_modif, Equipement['ID'] == id_modif)  # modif du dict associé à l'équipement
 
     def _ObtenirProchainID(self):
-        ID = self._nextID
-        self._nextID = self._nextID + 1                             # nb d'équipements dans a base de données
-        return ID                                                   # nouvel id (prochain numéro sur la liste)
 
-    def _VerifierDict(self, dictio):
+        db= TinyDB('fichier_conf.json')
+        print(db.all())
+        dernier_ID = db.all()[0]['dernier_ID_distribue']
+        print('dernierID', dernier_ID)
+        prochain_ID = int(dernier_ID) + 1
+        print('nextID', prochain_ID)
+        db.update({'dernier_ID_distribue': prochain_ID}, Query()['dernier_ID_distribue'] == dernier_ID)
+        return prochain_ID
+
+
+        #self._nextID = self._nextID + 1                             # nb d'équipements dans a base de données
+        #print('ID', ID)
+        #print('self.nextID', self._nextID)
+        #return self._nextID                                                   # nouvel id (prochain numéro sur la liste)
+
+    """def _VerifierDict(self, dictio):
         conforme = self.verifierChamps(dictio)
         # Vérifier que le contenu de chaque champ est conforme à ce qui est attendu
         for key, value in dictio:
@@ -68,4 +79,19 @@ class EquipementManager:
     def _verifierChamps(self, dictio)
     # verifier la présence de tous les champs et qu'aucun champs supplémentaire ne soit présent
     conforme = True
-    return conforme
+    return conforme"""
+
+
+manager = EquipementManager('DataBase_Equipement.json')
+
+data = {'CategorieEquipement': 'ECG',
+        'Marque': 'PierreSavard',
+        'Modele': 'L5509'}
+
+dic_request = {'CategorieEquipement': 'ECG',
+               'Marque': 'PierreSavard',
+               'Modele': 'blabla'}
+
+print(manager.RechercherEquipement(dic_request))
+
+
