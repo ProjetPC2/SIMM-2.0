10a7841c2e83d6c84eb310fb68e6ec45c5680c2c
Camille Larose <larose_camille@hotmail.ca>
2016-06-05 21:23:06 -0400

    Réorganisation du dictionnaire des databases et nouvelle méthode pour les ID

--- Base de données/Classes_v2 (Manager)/BonTravailManager.py
+++ Base de données/Classes_v2 (Manager)/BonTravailManager.py
@@ -1,65 +1,83 @@
 # coding=utf-8
 from tinydb import *
+from tinydb.operations import increment
 from yamlStorage import YAMLStorage
 import datetime
 import re
 
 
 class BonTravailManager:
-    def __init__(self, bdt_pathname, archive_id_bdt_pathname):
-        self._pathname = bdt_pathname                            # pathname de la base de données des bons de travail
-        self._archive_id_bdt_pathname = archive_id_bdt_pathname  # pathname de la base de données de l'archive des bdt
+    def __init__(self, bdt_pathname, equip_pathname):
+        self._pathname = bdt_pathname                           # pathname de la base de données des bons de travail
+        self._equip_pathname = equip_pathname
 
     def AjouterBonTravail(self, id_equipement, dictio):
+        # Ajout du bon de travail dans la base de données
         db = TinyDB(self._pathname, storage=YAMLStorage)        # data base des bons de travail
-        db_ID_bdT = TinyDB(self._archive_id_bdt_pathname, storage=YAMLStorage)
-        id_bdt = self._ObtenirProchainIDdeBDT(id_equipement)  # id du nouveau bon de travail
-        db_ID_bdT.insert({'id-eq': str(id_equipement), 'id-bdt': str(id_bdt), 'utilisation': True}) # ajout d'un nouveau bdt en cours
-        db.insert({'id-eq': str(id_equipement), 'id-bdt': str(id_bdt), 'data': dictio})  # ajout du nouveau bdt dans la db
+        id_bdt = self._ObtenirProchainIDdeBDT(id_equipement)    # id du nouveau bon de travail
+        dictio['ID-EQ'] = id_equipement
+        dictio['ID-BDT'] = id_bdt
+        db.insert(dictio)                                       # ajout du nouveau bdt dans la db
+
+        # Mise à jour du nombre de bons de travail pour cet équipement dans la base de données des équipements
+        db_equip = TinyDB(self._equip_pathname, storage=YAMLStorage)
+        Equipement = Query()
+        db_equip.update(increment('Nombre de bons de travail'), Equipement['ID'] == id_equipement)
+
 
     def SupprimerBonTravail(self, id_eq_supp, id_bdt_supp):
         BonTravail = Query()
         db = TinyDB(self._pathname, storage=YAMLStorage)        # data base des bons de travail
-        db.remove((BonTravail['id-eq'] == id_eq_supp) & (BonTravail['id-bdt'] == id_bdt_supp))  # suppression du bdt (???)
-        db_arch = TinyDB(self._archive_id_bdt_pathname, storage=YAMLStorage) # data base de l'archive
-        db_arch.update({'utilisation': False}, (BonTravail['id-eq'] == id_eq_supp) & (BonTravail['id-bdt'] == id_bdt_supp))
-
-        # **************************************************************************************************
-        # Voir avec Maxime et Alex:
-        # # lorsqu'on supprime un équipement, est-ce qu'on fait seulement que changer l'état de
-        # l'équipement, c-à-d changer le champ 'état de service' : Au rebut, dans le dictionnaire? Cela ferait en sorte
-        # qu'on garderait tous les équipements 'au rebut' dans la bdd, mais ce champ indiquerait si on les affiche ou
-        # non lorsqu'on fait un recherche. Pour les afficher, il faudrait préciser que l'équipement est au rebut.
-        #
-        # A quel point est-ce nécessaire d'avoir 2 bases de données : base de données + archive? On pourrait mettre dans
-        # la base de donnée directement un champ "utilisation" ou "statut" ...
-        # **************************************************************************************************
+        result = db.remove((BonTravail['ID-EQ'] == id_eq_supp) & (BonTravail['ID-BDT'] == id_bdt_supp))  # suppression du bdt
+        return result
 
     def RechercherBonTravail(self, regex_dict):
         db = TinyDB(self._pathname, storage=YAMLStorage)
-
         recherche = Query()
-        #définir les critères de recherche
-
-        result = db.search(recherche['id-eq'].matches(regex_dict['id-eq']) & recherche['id-bdt'].matches(regex_dict['id-bdt']))
+        firstEntry = True
+        for key, value in regex_dict.items():
+            if firstEntry:
+                queryUser = (recherche[key].matches(value))
+                firstEntry = False
+            else:
+                queryUser = (queryUser) & (recherche[key].matches(value))
+        result = db.search(queryUser)
         return result
 
     def ModifierBonTravail(self, id_eq_modif, id_bdt_modif, dict_modif):
         BonTravail = Query()
         db = TinyDB(self._pathname, storage=YAMLStorage)        # data base des bons de travail
         # modif du dict associé au bdt
-        db.update({'data': dict_modif}, (BonTravail['id-eq'] == id_eq_modif) & (BonTravail['id-bdt'] == id_bdt_modif))
+        db.update(dict_modif, (BonTravail['ID-EQ'] == id_eq_modif) & (BonTravail['ID-BDT'] == id_bdt_modif))
 
     def _ObtenirProchainIDdeBDT(self, id_equip):
-        BonTravail = Query()
-        db = TinyDB(self._archive_id_bdt_pathname, storage=YAMLStorage)   # data base de l'ARCHIVE des bdt
-        nb_bdt_pour_eq = db.count(BonTravail['id-eq'] == str(id_equip))  # nb de bdt pour cet équipement dans la db
-        print('count', nb_bdt_pour_eq)
-        # faire étape de vérification
-        new_id_bdt = nb_bdt_pour_eq + 1                         # nouvel id de bdt
-
+        Equipement = Query()
+        db = TinyDB(self._equip_pathname, storage=YAMLStorage)
+        equipement_ = db.get((Equipement['ID'] == id_equip))
+        nb_bdt_pour_eq = equipement_['Nombre de bons de travail']
+        # faire étape de vérification si l'équipement existe
+        if nb_bdt_pour_eq is None:                                  # vérifier si des bons ont déjà été enregistrés
+            new_id_bdt = 1
+        else:
+            new_id_bdt = nb_bdt_pour_eq + 1                         # nouvel id de bdt
         return new_id_bdt
 
+
+# TESTS
+manager = BonTravailManager('DataBase_BDT.json', 'DataBase_Equipement.json')
+
+data = {'Date': datetime.date.today(),                              # format de la date à vérifier
+        'Temps estime': '2-modif',
+        'Description de la situation': 'test-modif'}
+
+dic_request = {'Description de la situation': 'test',               # VÉRIFIER LA RECHERCHE POUR LES DATES...
+               'Temps estime': '2'}
+
+
+#manager.AjouterBonTravail(1, data)
+#manager.SupprimerBonTravail(1, 2)                  # id_supp en int
+#print(manager.RechercherBonTravail(dic_request))
+#manager.ModifierBonTravail(1, 2, data)                # id_modif en int
 
 
 
--- Base de données/Classes_v2 (Manager)/EquipementManager.py
+++ Base de données/Classes_v2 (Manager)/EquipementManager.py
@@ -1,57 +1,67 @@
 # coding=utf-8
 from tinydb import *
-from bbd_equipement import yamlStorage
+from yamlStorage import YAMLStorage
 import re
 
 
 class EquipementManager:
-    def __init__(self, pathname, archive_id_eq_pathname):
+    def __init__(self, pathname):
         self._pathname = pathname                               # pathname de la base de données des équipements
-        self._nextID = 0   # pathname de la base de données de l'archive des équip.
+        # self._nextID = 1   # pathname de la base de données de l'archive des équip.
+
         # nextID à mettre dans le fichier de configuration
 
     def AjouterEquipement(self, dictio):
-        #print('ouverture de la bdd')
-        db1 = TinyDB(self._pathname, storage=yamlStorage.YAMLStorage)        # data base des équipements
-        #print('Attribution du ID')
-        id_eq = self._ObtenirProchainID()                                   # id du nouvel équipement
-        #print(str(id_eq))
-        #print('Insertion de equipement')
+        db1 = TinyDB(self._pathname, storage=YAMLStorage)       # data base des équipements
+        id_eq = self._ObtenirProchainID()                       # id du nouvel équipement
         dictio['ID'] = id_eq
         # Verifier que tout ce qui est dans dictio est conforme à la forme d'un équipement et COMPLET
-        db1.insert(dictio)           # ajout du nouvel équipement dans la base de données
+        db1.insert(dictio)                                      # ajout du nouvel équipement dans la base de données
 
-    def SupprimerEquipement(self, id_supp):
+    def SupprimerEquipement(self, id_supp):                     # id_supp en int
         Equipement = Query()
-        db = TinyDB(self._pathname, storage=yamlStorage.YAMLStorage)        # data base des équipements
-        result = db.remove(Equipement['ID'] == id_supp)                              # suppression de l'équipement (à voir...)
+        db = TinyDB(self._pathname, storage=YAMLStorage)        # data base des équipements
+        result = db.remove(Equipement['ID'] == id_supp)         # suppression de l'équipement
         return result
 
     def RechercherEquipement(self, regex_dict):
-        db = TinyDB(self._pathname, storage=yamlStorage.YAMLStorage)
-
+        db = TinyDB(self._pathname, storage=YAMLStorage)
         recherche = Query()
-        
         firstEntry = True
-        for key,value in regex_dict:
+        for key, value in regex_dict.items():
             if firstEntry:
-                queryUser = recherche[key].matches(value)
+                queryUser = (recherche[key].matches(value))
                 firstEntry = False
             else:
-                queryUser = & recherche[key].matches(value)
+                queryUser = (queryUser) & (recherche[key].matches(value))
         result = db.search(queryUser)
         return result
 
-    def ModifierEquipement(self, id_modif, dict_modif):
+    def ModifierEquipement(self, id_modif, dict_modif):         # id_modif en int
         Equipement = Query()
-        db = TinyDB(self._pathname, storage=yamlStorage.YAMLStorage)        # data base des équipements
+        db = TinyDB(self._pathname, storage=YAMLStorage)        # data base des équipements
         # Verifier que tout ce qui est dans dict_modif est conforme à la forme d'un équipement et COMPLET
-        db.update(dict_modif, Equipement['ID'] == id_modif)  # modif du dict associé à l'équipement
+        db.update(dict_modif, Equipement['ID'] == id_modif)     # modif du dict associé à l'équipement
 
     def _ObtenirProchainID(self):
-        ID = self._nextID
-        self._nextID = self._nextID + 1                             # nb d'équipements dans a base de données
-        return ID                                                   # nouvel id (prochain numéro sur la liste)
+        db = TinyDB('fichier_conf.json')
+        #print(db.all())
+        dernier_ID = db.all()[0]['dernier_ID_distribue']
+        #print('dernierID', dernier_ID)
+        prochain_ID = int(dernier_ID) + 1
+        #print('nextID', prochain_ID)
+        db.update({'dernier_ID_distribue': prochain_ID}, Query()['dernier_ID_distribue'] == dernier_ID)
+        return prochain_ID
+
+
+    # À COMPLÉTER
+    """
+
+    def _verifierChamps(self, dictio):
+
+        dictio[]
+
+
 
     def _VerifierDict(self, dictio):
         conforme = self.verifierChamps(dictio)
@@ -69,3 +79,29 @@ class EquipementManager:
     # verifier la présence de tous les champs et qu'aucun champs supplémentaire ne soit présent
     conforme = True
     return conforme
+
+    """
+
+
+# TESTS
+manager = EquipementManager('DataBase_Equipement.json')
+
+data = {'CategorieEquipement': 'Equip-modif',
+        'Marque': 'Test-modif',
+        'Modele': 'Modifie',
+        'Nombre de bons de travail': 0}
+
+#dic_request = {'CategorieEquipement': 'ECG',
+#               'Marque': 'PierreSavard',
+#               'Modele': 'blabla'}
+
+dic_request = {'CategorieEquipement': 'ECG'}
+
+
+manager.AjouterEquipement(data)
+#manager.SupprimerEquipement(6)                     # id_supp en int
+#print(manager.RechercherEquipement(dic_request))
+#manager.ModifierEquipement(4, data)                 # id_modif en int
+
+
+
